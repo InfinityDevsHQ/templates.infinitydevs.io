1ad64b133171ca3d4d456ef2d76c1f9fceeeb12a
sheraz4196 <sheraztoor321@gmail.com>
2024-08-01 18:32:06 +0500

    fix: responsiveness

--- components/landing-page/latest-templates.tsx
+++ components/landing-page/latest-templates.tsx
@@ -1,10 +1,16 @@
 import { Templates } from "$/constants";
-import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 import TemplateCard from "./_components/template-card";
 
 export default function LatestTemplates() {
   return (
-    <section className="py-16 p-6">
+    <section className="py-16 p-6 relative">
       <div className="max-w-screen-xl w-full mx-auto text-center flex flex-col gap-8">
         <h1 className="text-white text-xl lg:text-4xl font-bold">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary-blue to-primary-purple">
@@ -15,11 +21,13 @@ export default function LatestTemplates() {
         <Carousel>
           <CarouselContent>
             {Templates.map((template) => (
-              <CarouselItem className="basis-1/3" key={template.id}>
+              <CarouselItem className="lg:basis-1/3" key={template.id}>
                 <TemplateCard template={template} />
               </CarouselItem>
             ))}
           </CarouselContent>
+          <CarouselNext className="right-10" />
+          <CarouselPrevious className="left-10" />
         </Carousel>
       </div>
     </section>
