36cb00d6e7d13af63ef5a06835458d4fe1dd5632
sheraz4196 <sheraztoor321@gmail.com>
2024-08-05 17:14:22 +0500

    fix: text casing

--- components/landing-page/all-templates.tsx
+++ components/landing-page/all-templates.tsx
@@ -5,7 +5,7 @@ export default function AllTemplates() {
   return (
     <section className="pt-16 pb-8 p-6" id="templates">
       <div className="max-w-screen-xl w-full mx-auto text-center flex flex-col gap-8">
-        <h1 className="text-white text-xl lg:text-4xl font-bold">
+        <h1 className="text-white text-xl lg:text-4xl font-bold uppercase">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary-blue to-primary-purple">
             Our
           </span>{" "}
