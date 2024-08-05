c1c992e9a25db0c0c675688332c7045441334f9f
sheraz4196 <sheraztoor321@gmail.com>
2024-08-05 16:13:43 +0500

    fix: gaps

--- components/landing-page/latest-templates.tsx
+++ components/landing-page/latest-templates.tsx
@@ -31,7 +31,7 @@ export default function LatestTemplates() {
 
   return (
     <section className="py-16 p-6 relative mb-16">
-      <div className="max-w-screen-xl w-full mx-auto text-center flex flex-col">
+      <div className="max-w-screen-xl w-full mx-auto text-center flex flex-col gap-8">
         <h1 className="text-white text-xl lg:text-4xl font-bold">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary-blue to-primary-purple">
             Latest
