b58851a638cd397c02d0ab8395a3369ac8c9d4ea
sheraz4196 <sheraztoor321@gmail.com>
2024-08-05 12:16:41 +0500

    chore: replace images

--- components/landing-page/_components/template-card.tsx
+++ components/landing-page/_components/template-card.tsx
@@ -10,7 +10,7 @@ export default function TemplateCard({ template }: { template: TemplateCard }) {
           alt="Template Thumbnail"
           width={348}
           height={484}
-          className="w-full h-full bg-white"
+          className="w-full h-full object-fill"
         />
 
         <div className="flex items-center p-4 bg-gradient-to-r from-primary/90 to-primary-dark/90 absolute w-full bottom-0 left-0">
