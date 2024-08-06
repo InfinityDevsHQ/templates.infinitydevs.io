87a718e2f220d8afe732e25c17e375cb9d2b9a1a
sheraz4196 <sheraztoor321@gmail.com>
2024-08-06 19:23:09 +0500

    added: encryption and totals section

--- components/forms/checkout-form.tsx
+++ components/forms/checkout-form.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import { Lock } from "lucide-react";
 import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,7 +13,9 @@ import {
 } from "../ui/form";
 import { z } from "zod";
 import { Input } from "../ui/input";
-const checkoutFormSchema = z.object({
+import PayPalPayment from "./checkout-forms/paypal-payment";
+import { Button } from "../ui/button";
+const cardPaymentSchema = z.object({
   email: z.string().email({ message: "Please enter a valid Email Address" }),
   fullName: z.string().min(1, { message: "Please Enter Your Name" }),
   taxIdNumber: z.number().optional(),
@@ -26,38 +27,40 @@ const checkoutFormSchema = z.object({
   address: z.string(),
   postalCode: z.string(),
 });
+
 export default function CheckoutForm() {
-  const form = useForm<z.infer<typeof checkoutFormSchema>>({
-    resolver: zodResolver(checkoutFormSchema),
+  const card = useForm<z.infer<typeof cardPaymentSchema>>({
+    resolver: zodResolver(cardPaymentSchema),
     defaultValues: {
       email: "",
     },
   });
+
   return (
-    <Form {...form}>
-      <form className="bg-white">
-        <div className="max-w-70 w-full mx-auto px-8 pb-20 pt-12 md:pt-32">
+    <div className="bg-white">
+      <div className="max-w-70 w-full mx-auto px-8 pb-20 pt-12 md:pt-32">
+        <div>
           <div>
-            <div>
-              <Tabs defaultValue="Pay by Card" className="w-full">
-                <TabsList className="w-full mb-8 0.5 rounded-lg">
-                  <TabsTrigger
-                    className="w-1/2 py-1.5 px-4 text-sm"
-                    value="Pay by Card"
-                  >
-                    Pay by Card
-                  </TabsTrigger>
-                  <TabsTrigger
-                    className="w-1/2 py-1.5 px-4 text-sm"
-                    value="Pay With Paypal"
-                  >
-                    Password
-                  </TabsTrigger>
-                </TabsList>
-                <TabsContent value="Pay by Card">
-                  <div>
+            <Tabs defaultValue="Pay by Card" className="w-full">
+              <TabsList className="w-full mb-8 0.5 rounded-lg">
+                <TabsTrigger
+                  className="w-1/2 py-1.5 px-4 text-sm"
+                  value="Pay by Card"
+                >
+                  Pay by Card
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-1/2 py-1.5 px-4 text-sm"
+                  value="Pay With Paypal"
+                >
+                  Pay With Paypal
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="Pay by Card">
+                <Form {...card}>
+                  <form>
                     <FormField
-                      control={form.control}
+                      control={card.control}
                       name="email"
                       render={({ field }) => (
                         <FormItem className="mb-6">
@@ -76,7 +79,7 @@ export default function CheckoutForm() {
                       )}
                     />
                     <FormField
-                      control={form.control}
+                      control={card.control}
                       name="fullName"
                       render={({ field }) => (
                         <FormItem className="mb-6">
@@ -91,7 +94,7 @@ export default function CheckoutForm() {
                       )}
                     />
                     <FormField
-                      control={form.control}
+                      control={card.control}
                       name="taxIdNumber"
                       render={({ field }) => (
                         <FormItem className="mb-6">
@@ -109,7 +112,7 @@ export default function CheckoutForm() {
                       )}
                     />
                     <FormField
-                      control={form.control}
+                      control={card.control}
                       name="discountCode"
                       render={({ field }) => (
                         <FormItem className="mb-6">
@@ -126,87 +129,37 @@ export default function CheckoutForm() {
                         </FormItem>
                       )}
                     />
-                  </div>
-                </TabsContent>
-                <TabsContent value="Pay With Paypal">
-                  <div>
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem className="mb-6">
-                          <FormLabel className="mb-3 text-sm checkout-form-label">
-                            Email
-                          </FormLabel>
-                          <FormControl>
-                            <Input
-                              className="checkout-form-input"
-                              placeholder="eg. john@example.com"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage className="text-sm mt-3" />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="fullName"
-                      render={({ field }) => (
-                        <FormItem className="mb-6">
-                          <FormLabel className="mb-3 text-sm checkout-form-label">
-                            Full Name
-                          </FormLabel>
-                          <FormControl>
-                            <Input className="checkout-form-input" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="taxIdNumber"
-                      render={({ field }) => (
-                        <FormItem className="mb-6">
-                          <FormLabel className="mb-3 text-sm checkout-form-label">
-                            Tax ID number{" "}
-                            <span className="text-xs text-gray-500">
-                              (optional)
-                            </span>
-                          </FormLabel>
-                          <FormControl>
-                            <Input className="checkout-form-input" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="discountCode"
-                      render={({ field }) => (
-                        <FormItem className="mb-6">
-                          <FormLabel className="mb-3 text-sm checkout-form-label">
-                            Tax ID number{" "}
-                            <span className="text-xs text-gray-500">
-                              (optional)
-                            </span>
-                          </FormLabel>
-                          <FormControl>
-                            <Input className="checkout-form-input" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </TabsContent>
-              </Tabs>
-            </div>
+                    <div className="my-6">
+                      <div className="flex items-center justify-center py-2">
+                        <span>Subtotal</span>
+                        <span className="ml-auto">$50</span>
+                      </div>
+                      <div className="flex items-center justify-center py-2 font-medium">
+                        <span>Total</span>
+                        <span className="ml-auto">$50</span>
+                      </div>
+                    </div>
+                    <Button className="w-full py-2.5 bg-violet-500 mb-8">
+                      Pay $50.00
+                    </Button>
+                    <Button
+                      type="button"
+                      className="w-full bg-gray-300 text-zinc-600"
+                      disabled
+                    >
+                      <Lock className="h-6 w-6 mr-2" />
+                      <span>Payments are secure and encrypted</span>
+                    </Button>
+                  </form>
+                </Form>
+              </TabsContent>
+              <TabsContent value="Pay With Paypal">
+                <PayPalPayment />
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
-      </form>
-    </Form>
+      </div>
+    </div>
   );
 }
---
+++ components/forms/checkout-forms/paypal-payment.tsx
@@ -0,0 +1,124 @@
+import { Button } from "$/components/ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "$/components/ui/form";
+import { Input } from "$/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Lock } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+const paypalSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid Email Address" }),
+  fullName: z.string().min(1, { message: "Please Enter Your Name" }),
+  taxIdNumber: z.number().optional(),
+  discountCode: z
+    .string()
+    .min(1, { message: "Please Enter a discount code" })
+    .optional(),
+  country: z.string(),
+  address: z.string(),
+  postalCode: z.string(),
+});
+export default function PayPalPayment() {
+  const form = useForm<z.infer<typeof paypalSchema>>({
+    resolver: zodResolver(paypalSchema),
+  });
+  return (
+    <Form {...form}>
+      <form>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="mb-6">
+              <FormLabel className="mb-3 text-sm checkout-form-label">
+                Email
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="checkout-form-input"
+                  placeholder="eg. john@example.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-sm mt-3" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="fullName"
+          render={({ field }) => (
+            <FormItem className="mb-6">
+              <FormLabel className="mb-3 text-sm checkout-form-label">
+                Full Name
+              </FormLabel>
+              <FormControl>
+                <Input className="checkout-form-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="taxIdNumber"
+          render={({ field }) => (
+            <FormItem className="mb-6">
+              <FormLabel className="mb-3 text-sm checkout-form-label">
+                Tax ID number{" "}
+                <span className="text-xs text-gray-500">(optional)</span>
+              </FormLabel>
+              <FormControl>
+                <Input className="checkout-form-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="discountCode"
+          render={({ field }) => (
+            <FormItem className="mb-6">
+              <FormLabel className="mb-3 text-sm checkout-form-label">
+                Tax ID number{" "}
+                <span className="text-xs text-gray-500">(optional)</span>
+              </FormLabel>
+              <FormControl>
+                <Input className="checkout-form-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="my-6">
+          <div className="flex items-center justify-center py-2">
+            <span>Subtotal</span>
+            <span className="ml-auto">$50</span>
+          </div>
+          <div className="flex items-center justify-center py-2 font-medium">
+            <span>Total</span>
+            <span className="ml-auto">$50</span>
+          </div>
+        </div>
+        <Button type="submit" className="w-full bg-yellow-500 mb-8">
+          Pay With Paypal
+        </Button>
+        <Button
+          type="button"
+          className="w-full bg-gray-300 text-zinc-600"
+          disabled
+        >
+          <Lock className="h-6 w-6 mr-2" />
+          <span>Payments are secure and encrypted</span>
+        </Button>
+      </form>
+    </Form>
+  );
+}
