bb6b3165f20892d234f015a51404e320d446a956
sheraz4196 <sheraztoor321@gmail.com>
2024-08-07 16:31:39 +0500

    fix: hide header and footer on checout page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 import DesktopNav from "./navigation";
 import smoothScroll from "../utils/smoothScroll";
+import { usePathname } from "next/navigation";
 
 export type HeaderProps = {
   className?: string;
@@ -14,96 +15,110 @@ export type HeaderProps = {
 
 export default function Header({ className }: HeaderProps) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const pathname = usePathname();
   const sidebarVariants = {
     open: { x: 0 },
     closed: { x: "100%" },
   };
+  const NoHeaderRotes = ["/checkout"];
 
   return (
-    <header
-      className={`flex z-50 left-0 items-end justify-between bg-transparent w-full px-4 md:px-10 lg:px-12 py-4 lg:py-8 ${className}`}
-    >
-      <Link href={"/"}>
-        <Image
-          src={"/logo-light.png"}
-          width={237}
-          height={48}
-          className="hidden lg:inline-block"
-          alt="Infinity Devs Logo"
-        />
-        <Image
-          src={"/logo-light.png"}
-          width={170}
-          height={170}
-          className="lg:hidden"
-          alt="Infinity Devs Logo"
-        />
-      </Link>
+    <>
+      {NoHeaderRotes.map((route) => (
+        <>
+          {pathname.includes(route) ? (
+            <></>
+          ) : (
+            <header
+              className={`flex z-50 left-0 items-end justify-between bg-transparent w-full px-4 md:px-10 lg:px-12 py-4 lg:py-8 ${className}`}
+            >
+              <Link href={"/"}>
+                <Image
+                  src={"/logo-light.png"}
+                  width={237}
+                  height={48}
+                  className="hidden lg:inline-block"
+                  alt="Infinity Devs Logo"
+                />
+                <Image
+                  src={"/logo-light.png"}
+                  width={170}
+                  height={170}
+                  className="lg:hidden"
+                  alt="Infinity Devs Logo"
+                />
+              </Link>
 
-      <div>
-        <div className="lg:hidden">
-          <button
-            aria-label="Sidebar-menu-open-button"
-            type="button"
-            className="text-white font-bold pt-1 mt-1"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <Menu className="w-full" />
-          </button>
-          <AnimatePresence>
-            {sidebarOpen && (
-              <motion.div
-                className="flex fixed top-0 z-50 right-0 h-screen w-screen"
-                initial="closed"
-                animate="open"
-                exit="closed"
-                variants={sidebarVariants}
-              >
-                <div className="bg-gray-900 h-full w-full">
-                  <div className="px-6 py-4">
-                    <button
-                      aria-label="Sidebar menu close button"
-                      type="button"
-                      className="text-white text-4xl w-full flex justify-end"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <X size={32} />
-                    </button>
-                  </div>
-                  <nav className="flex items-center justify-center text-zinc-300 text-2xl capitalize font-bold h-full">
-                    <ol className="h-full flex flex-col items-center justify-center gap-14 mb-24">
-                      <li>
-                        <Link href={"https://infinitydevs.io/"}>Home</Link>
-                      </li>
-                      <li>
-                        <Link href={"#"}>Templates</Link>
-                      </li>
-                      <li>
-                        <Link href={"/portfolio"}>Portfolio</Link>
-                      </li>
-                      <li>
-                        <button
-                          type="button"
-                          onClick={() => {
-                            smoothScroll({ sectionId: "contact-us" });
-                            setSidebarOpen(false);
-                          }}
-                        >
-                          Contact Us
-                        </button>
-                      </li>
-                      {/* <li>
+              <div>
+                <div className="lg:hidden">
+                  <button
+                    aria-label="Sidebar-menu-open-button"
+                    type="button"
+                    className="text-white font-bold pt-1 mt-1"
+                    onClick={() => setSidebarOpen(true)}
+                  >
+                    <Menu className="w-full" />
+                  </button>
+                  <AnimatePresence>
+                    {sidebarOpen && (
+                      <motion.div
+                        className="flex fixed top-0 z-50 right-0 h-screen w-screen"
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
+                        variants={sidebarVariants}
+                      >
+                        <div className="bg-gray-900 h-full w-full">
+                          <div className="px-6 py-4">
+                            <button
+                              aria-label="Sidebar menu close button"
+                              type="button"
+                              className="text-white text-4xl w-full flex justify-end"
+                              onClick={() => setSidebarOpen(false)}
+                            >
+                              <X size={32} />
+                            </button>
+                          </div>
+                          <nav className="flex items-center justify-center text-zinc-300 text-2xl capitalize font-bold h-full">
+                            <ol className="h-full flex flex-col items-center justify-center gap-14 mb-24">
+                              <li>
+                                <Link href={"https://infinitydevs.io/"}>
+                                  Home
+                                </Link>
+                              </li>
+                              <li>
+                                <Link href={"#"}>Templates</Link>
+                              </li>
+                              <li>
+                                <Link href={"/portfolio"}>Portfolio</Link>
+                              </li>
+                              <li>
+                                <button
+                                  type="button"
+                                  onClick={() => {
+                                    smoothScroll({ sectionId: "contact-us" });
+                                    setSidebarOpen(false);
+                                  }}
+                                >
+                                  Contact Us
+                                </button>
+                              </li>
+                              {/* <li>
                         <Link href={"/"}></Link>
                       </li> */}
-                    </ol>
-                  </nav>
+                            </ol>
+                          </nav>
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
                 </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
-      </div>
-      <DesktopNav className="hidden lg:flex justify-between" />
-    </header>
+              </div>
+              <DesktopNav className="hidden lg:flex justify-between" />
+            </header>
+          )}
+        </>
+      ))}
+    </>
   );
 }
--- components/gernal/footer.tsx
+++ components/gernal/footer.tsx
@@ -1,48 +1,62 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Linkedin, Twitter, Instagram } from "lucide-react";
+import { usePathname } from "next/navigation";
 export default function Footer() {
+  const pathName = usePathname();
+  const NoFooterRoutes = ["/checkout"];
   return (
-    <footer className="p-8 mx-auto max-w-screen-2xl">
-      <div className="p-12 rounded-3xl bg-gradient-to-r from-primary-blue/10 via-light-velvet/10 to-primary-purple/10">
-        <div className="flex flex-col gap-4 lg:flex-row items-center  justify-between border-b-2 border-lines pb-4">
-          <div className="flex items-center justify-center">
-            <Image
-              src={"/logo-light.png"}
-              alt="Infinity Devs lOGO"
-              width={237}
-              height={48}
-            />
-          </div>
-          {/* <DesktopNav /> */}
-          <div className="flex gap-4 text-white">
-            <Link
-              href={"https://www.linkedin.com/company/infinitydevshq/"}
-              target="_blank"
-              className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2  md:p-3 rounded-full"
-            >
-              <Linkedin size={28} fill="transparent" />
-            </Link>
-            <Link
-              href={"https://twitter.com/InfinityDevsHQ"}
-              target="_blank"
-              className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2  md:p-3 rounded-full"
-            >
-              <Twitter size={28} fill="transparent" />
-            </Link>
-            <Link
-              href={"https://www.instagram.com/infinitydevshq"}
-              target="_blank"
-              className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2   md:p-3 rounded-full"
-            >
-              <Instagram size={28} fill="transparent" />
-            </Link>
-          </div>
-        </div>
-        <p className="main-description text-white text-center pt-5 lg:text-xl font-medium">
-          Infinity Devs &copy; 2024, All Rights Reserved
-        </p>
-      </div>
-    </footer>
+    <>
+      {NoFooterRoutes.map((route) => (
+        <>
+          {pathName.includes(route) ? (
+            <></>
+          ) : (
+            <footer className="p-8 mx-auto max-w-screen-2xl">
+              <div className="p-12 rounded-3xl bg-gradient-to-r from-primary-blue/10 via-light-velvet/10 to-primary-purple/10">
+                <div className="flex flex-col gap-4 lg:flex-row items-center  justify-between border-b-2 border-lines pb-4">
+                  <div className="flex items-center justify-center">
+                    <Image
+                      src={"/logo-light.png"}
+                      alt="Infinity Devs lOGO"
+                      width={237}
+                      height={48}
+                    />
+                  </div>
+                  {/* <DesktopNav /> */}
+                  <div className="flex gap-4 text-white">
+                    <Link
+                      href={"https://www.linkedin.com/company/infinitydevshq/"}
+                      target="_blank"
+                      className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2  md:p-3 rounded-full"
+                    >
+                      <Linkedin size={28} fill="transparent" />
+                    </Link>
+                    <Link
+                      href={"https://twitter.com/InfinityDevsHQ"}
+                      target="_blank"
+                      className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2  md:p-3 rounded-full"
+                    >
+                      <Twitter size={28} fill="transparent" />
+                    </Link>
+                    <Link
+                      href={"https://www.instagram.com/infinitydevshq"}
+                      target="_blank"
+                      className="common-gradient hover:-translate-y-1.5 transition-all ease-in-out duration-300 p-2   md:p-3 rounded-full"
+                    >
+                      <Instagram size={28} fill="transparent" />
+                    </Link>
+                  </div>
+                </div>
+                <p className="main-description text-white text-center pt-5 lg:text-xl font-medium">
+                  Infinity Devs &copy; 2024, All Rights Reserved
+                </p>
+              </div>
+            </footer>
+          )}
+        </>
+      ))}
+    </>
   );
 }
