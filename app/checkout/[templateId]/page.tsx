5f9c89ee105b0838016708a764c1bc3b5bb6e975
sheraz4196 <sheraztoor321@gmail.com>
2024-08-07 17:02:19 +0500

    feat: site logo

--- app/checkout/[templateId]/page.tsx
+++ app/checkout/[templateId]/page.tsx
@@ -13,49 +13,58 @@ export default function TemplateCheckout({
   );
   if (!template) return notFound();
   return (
-    <section className="w-full grid lg:grid-cols-2 min-h-screen">
-      <div className="bg-gray-50 text-gray-900">
-        <div className="mx-auto max-w-[512px] px-8 pb-12 pt-32 md:pb-32">
-          <div>
-            <div className="mb-8">
-              <div className="flex items-start">
-                <h1 className="text-2xl font-medium">
-                  <span>{template?.title}</span>
-                </h1>
-                <p className="ml-auto flex-shrink-1">
-                  <span className="ml-2 text-xl">
-                    <span className="text-xl text-gray-400 font-semibold">
-                      {template?.price || "Free"}
-                    </span>
-                  </span>
-                </p>
-              </div>
-            </div>
-            <div className="relative rounded-lg mb-8">
-              <div className="rounded-lg  flex items-center justify-center">
-                <Image
-                  src={template?.imgUrl || ""}
-                  width={448}
-                  height={336}
-                  alt="Template Thumbnail"
-                />
-              </div>
-            </div>
+    <section>
+      <div className="w-full grid lg:grid-cols-2 relative">
+        <Image
+          src="/logo-light.png"
+          alt="Infinity devs logo"
+          className="ml-6 mt-6 absolute top-1 left-1"
+          width={144}
+          height={24}
+        />
+        <div className="bg-gray-50 text-gray-900">
+          <div className="mx-auto max-w-[512px] px-8 pb-12 pt-32 md:pb-32">
             <div>
+              <div className="mb-8">
+                <div className="flex items-start">
+                  <h1 className="text-2xl font-medium">
+                    <span>{template?.title}</span>
+                  </h1>
+                  <p className="ml-auto flex-shrink-1">
+                    <span className="ml-2 text-xl">
+                      <span className="text-xl text-gray-400 font-semibold">
+                        {template?.price || "Free"}
+                      </span>
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className="relative rounded-lg mb-8">
+                <div className="rounded-lg  flex items-center justify-center">
+                  <Image
+                    src={template?.imgUrl || ""}
+                    width={448}
+                    height={336}
+                    alt="Template Thumbnail"
+                  />
+                </div>
+              </div>
               <div>
-                <p className="mb-4">{template?.description}</p>
-                <p>
-                  Live Demo at{" "}
-                  <Link className="text-primary-blue" href={template?.link}>
-                    {template.link}
-                  </Link>
-                </p>
+                <div>
+                  <p className="mb-4">{template?.description}</p>
+                  <p>
+                    Live Demo at{" "}
+                    <Link className="text-primary-blue" href={template?.link}>
+                      {template.link}
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <CheckoutForm />
       </div>
-      <CheckoutForm />
     </section>
   );
 }
