b1b34bab22d15afd60931254e698414797a28055
sheraz4196 <sheraztoor321@gmail.com>
2024-08-07 12:12:21 +0500

    feat: new tabs

--- components/forms/checkout-forms/card-payment.tsx
+++ components/forms/checkout-forms/card-payment.tsx
@@ -10,8 +10,9 @@ import {
 import { Input } from "$/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Lock } from "lucide-react";
+import { CreditCard, Lock } from "lucide-react";
 import { z } from "zod";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "$/components/ui/tabs";
 const cardPaymentSchema = z.object({
   email: z.string().email({ message: "Please enter a valid Email Address" }),
   fullName: z.string().min(1, { message: "Please Enter Your Name" }),
@@ -54,21 +55,90 @@ export default function CardPaymentForm() {
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="fullName"
-          render={({ field }) => (
-            <FormItem className="mb-6">
-              <FormLabel className="mb-3 text-sm checkout-form-label">
-                Full Name
-              </FormLabel>
-              <FormControl>
-                <Input className="checkout-form-input" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+
+        <Tabs className="mb-6" defaultValue="card">
+          <TabsList className="bg-transparent w-full gap-2 mb-8">
+            <TabsTrigger
+              className="flex-col p-3 border shadow-none border-gray-200 bg-white justify-start items-start w-full text-gray-600 data-[state=active]:text-violet-600 data-[state=active]:border-violet-600 rounded-lg"
+              value="card"
+            >
+              <span>
+                <CreditCard className="h-3 w-3" />
+              </span>
+              <span className="pt-1 text-sm">Card</span>
+            </TabsTrigger>
+            <TabsTrigger
+              className="flex-col p-3 border shadow-none border-gray-200 bg-white justify-start items-start w-full text-gray-600 data-[state=active]:text-violet-600 data-[state=active]:border-violet-600 rounded-lg"
+              value="alipay"
+            >
+              <span>
+                <CreditCard className="h-3 w-3" />
+              </span>
+              <span className="pt-1 text-sm">Alipay</span>
+            </TabsTrigger>
+            <TabsTrigger
+              className="flex-col p-3 border shadow-none border-gray-200 bg-white justify-start items-start w-full text-gray-600 data-[state=active]:text-violet-600 data-[state=active]:border-violet-600 rounded-lg"
+              value="we-chat-pay"
+            >
+              <span>
+                <CreditCard className="h-3 w-3" />
+              </span>
+              <span className="pt-1 text-sm">WeChat Pay</span>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="card">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem className="mb-6">
+                  <FormLabel className="mb-3 text-sm checkout-form-label">
+                    Cardholder name
+                  </FormLabel>
+                  <FormControl>
+                    <Input className="checkout-form-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </TabsContent>
+          <TabsContent value="alipay">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem className="mb-6">
+                  <FormLabel className="mb-3 text-sm checkout-form-label">
+                    Full Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input className="checkout-form-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </TabsContent>
+          <TabsContent value="we-chat-pay">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem className="mb-6">
+                  <FormLabel className="mb-3 text-sm checkout-form-label">
+                    Full Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input className="checkout-form-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </TabsContent>
+        </Tabs>
+
         <FormField
           control={form.control}
           name="taxIdNumber"
